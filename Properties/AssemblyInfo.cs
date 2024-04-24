dbfdd7d3fc6add939fab1bb4379c2ccceedbf7fa
Lou_Franc0 <caputo.francesco001@gmail.com>
2024-04-24 17:01:12 +0200

    CssCshtmlpaginevarie

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("Pizzeria")]
+[assembly: AssemblyTitle("EcommerceChitarre")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pizzeria")]
+[assembly: AssemblyProduct("EcommerceChitarre")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
